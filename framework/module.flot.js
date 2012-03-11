5967d9c847f7ff49a09fd9c86e8bf4b3b32643c7
woho <woho@woho.woho>
2012-03-11 16:48:27 +0100

    fix jquery loading in jqplot and flot

--- framework/module.flot.js
+++ framework/module.flot.js
@@ -1,5 +1,6 @@
 (function() {
     if (typeof jQuery === 'undefined') {
+        window.jQuery = true;
         ws.module.loadScript('framework/flot/jquery.min.js');
     }
     ws.module.loadScript('framework/flot/jquery.flot.min.js');
--- framework/module.jqplot.js
+++ framework/module.jqplot.js
@@ -1,6 +1,7 @@
 (function() {
     ws.module.loadCSS('framework/jqplot/jquery.jqplot.min.css');
     if (typeof jQuery === 'undefined') {
+        window.jQuery = true;
         ws.module.loadScript('framework/jqplot/jquery.min.js');
     }
     ws.module.loadScript('framework/jqplot/jquery.jqplot.min.js');
